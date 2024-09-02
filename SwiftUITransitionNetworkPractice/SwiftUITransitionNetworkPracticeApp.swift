9dbf18bbf1801cc0abc06e3520e957cc0e0ed859
seongmin kim <k2417000@naver.com>
2024-09-02 15:33:02 +0900

    Initial Commit

---
+++ SwiftUITransitionNetworkPractice/SwiftUITransitionNetworkPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITransitionNetworkPracticeApp.swift
+//  SwiftUITransitionNetworkPractice
+//
+//  Created by 김성민 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITransitionNetworkPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
