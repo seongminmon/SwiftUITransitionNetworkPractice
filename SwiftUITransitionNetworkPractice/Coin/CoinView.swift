fc322616bd58772c83df546deadfeac6aa46e25c
seongmin kim <k2417000@naver.com>
2024-09-05 16:44:10 +0900

    코인 뷰 생성

---
+++ SwiftUITransitionNetworkPractice/Coin/CoinView.swift
@@ -0,0 +1,23 @@
+//
+//  CoinView.swift
+//  SwiftUITransitionNetworkPractice
+//
+//  Created by 김성민 on 9/5/24.
+//
+
+import SwiftUI
+
+struct CoinView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+            }
+            .navigationTitle("Coin")
+        }
+    }
+}
+
+#Preview {
+    CoinView()
+}
