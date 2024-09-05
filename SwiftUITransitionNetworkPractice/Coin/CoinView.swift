e8c80e71643a2cf77930dc7ecdc068053ee65073
seongmin kim <k2417000@naver.com>
2024-09-05 17:07:36 +0900

    코인 뷰 UI 그리기

--- SwiftUITransitionNetworkPractice/Coin/CoinView.swift
+++ SwiftUITransitionNetworkPractice/Coin/CoinView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct CoinView: View {
+    
     var body: some View {
         NavigationView {
             ScrollView {
-                
+                bannerView()
+                LazyVStack {
+                    ForEach(0..<10) { item in
+                        coinCell()
+                    }
+                }
             }
             .navigationTitle("Coin")
         }
+    }
+    
+    func bannerView() -> some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(.blue.gradient)
+            .frame(maxWidth: .infinity)
+            .frame(height: 150)
+            .overlay(alignment: .leading) {
+                Circle()
+                    .fill(.white.opacity(0.3))
+                    .scaleEffect(2)
+                    .offset(x: -50)
+            }
+            .overlay(alignment: .bottomLeading) {
+                VStack(alignment: .leading) {
+                    Text("11111")
+                    Text("22222")
+                        .font(.title)
+                        .bold()
+                }
+                .foregroundStyle(.white)
+                .padding()
+            }
+            .padding()
+    }
+    
+    func coinCell() -> some View {
+        HStack {
+            VStack {
+                Text("1111")
+                    .bold()
+                Text("2222")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+            Spacer()
+            Text("3333")
+        }
+        .padding()
     }
 }
 
